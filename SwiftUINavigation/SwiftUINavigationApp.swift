a14fc0d268c1dded745550eeb53eea8d8ef4bf0e
Sabricetin <sabriyazilim@gmail.com>
2024-09-28 13:56:20 +0300

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Sabri Çetin on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
